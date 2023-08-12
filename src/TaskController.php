f75677cb1a13d4793d28492be2a2d5e7ca87d0b9
Bryan Eriksson <bryaneriksson@uvic.ca>
2023-08-12 14:57:32 -0700

    added errorhandle

--- src/TaskController.php
+++ src/TaskController.php
@@ -18,7 +18,9 @@ class TaskController
                 
                 $data = (array) json_decode(file_get_contents("php://input"), true);
                 
-                var_dump($data);
+                $id = $this->gateway->create($data);
+
+                $this->respondCreated($id);
                 
             } else {
                 
@@ -64,6 +66,12 @@ class TaskController
     {
         http_response_code(404);
         echo json_encode(["message" => "Task with ID $id not found"]);
+    }
+
+    private function respondCreated(string $id): void
+    {
+        http_response_code(201);
+        echo json_encode(["message" => "Task created", "id" => $id]);
     }
 }
 
